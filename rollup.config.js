f975e0c8860c96cead31857cc0a8ff44b44ba8b3
t0ri <victoriarosemurray@gmail.com>
2019-11-12 15:06:28 -0800

    updated to add es6 modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,21 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'umd/strings.js',
-    format: 'umd',
-    name: 'strings',
+import { terser } from 'rollup-plugin-terser'
+
+export default [
+  {
+    input: 'src/index.js',
+    plugins: [terser()],
+    output: {
+      file: 'umd/strings.js',
+      format: 'umd',
+      name: 'strings',
+      esModule: false,
+    },
   },
-}
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'esm/strings.js',
+      format: 'esm',
+    },
+  },
+]
